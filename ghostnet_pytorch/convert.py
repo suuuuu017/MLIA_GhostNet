5e636495d89004b1b79580c13e6683cf71464e62
btufts <bt3qzd@virginia.edu>
2022-12-07 14:31:14 -0500

    photo converting

--- ghostnet_pytorch/convert.py
+++ ghostnet_pytorch/convert.py
@@ -2,19 +2,26 @@ import SimpleITK as sitk
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+from PIL import Image
 
 # for file in os.listdir("/mydir"):
 #     if file.endswith(".txt"):
 #         print(os.path.join("/mydir", file))
 
-rootdir = '/Users/suuuuu017/PycharmProjects/Efficient-AI-Backbones-master/ghostnet_pytorch/Classification_data'
+#rootdir = '/Users/suuuuu017/PycharmProjects/Efficient-AI-Backbones-master/ghostnet_pytorch/Classification_data'
+rootdir = '/home/bt3qzd/MLIA_GhostNet/ghostnet_pytorch/Classification_data'
 
-datadir = '/Users/suuuuu017/PycharmProjects/Efficient-AI-Backbones-master/ghostnet_pytorch/data'
-subdir = ['train/', 'val/']
+#datadir = '/Users/suuuuu017/PycharmProjects/Efficient-AI-Backbones-master/ghostnet_pytorch/data'
+datadir = '/home/bt3qzd/MLIA_GhostNet/ghostnet_pytorch/data/'
+subdirarr = ['train/', 'val/']
 subsubdir = ['Diseased/', 'Healthy/']
 
 subI = 0
 subsubI = 0
+
+i = 0
+j = 0
+k = 0
 
 for subdir, dirs, files in os.walk(rootdir):
     if dirs:
@@ -27,12 +34,26 @@ for subdir, dirs, files in os.walk(rootdir):
         if file.endswith(".mhd"):
             # print(os.path.join(subdir, file))
             itk_image = sitk.ReadImage(os.path.join(subdir, file))
-            image_array = np.array(sitk.GetArrayViewFromImage(itk_image))
-            path = os.path.join(datadir, subdir[subI], subsubdir[subsubI])
-            if not (os.path.exists(path)):
-                np.save(path, image_array)
+            image_array = np.array(sitk.GetArrayFromImage(itk_image))
+            path = datadir+subdirarr[i]+''+subsubdir[j]+'file'+str(k)+'.jpeg'
+            print(image_array.shape)
+            if len(image_array.shape) > 2:
+                image_array = image_array[0, :]
+            im = Image.fromarray(image_array*255)
+            im = im.convert("L")
+            im.save(path)
+            k += 1
+            # if not (os.path.exists(path)):
+            #     np.save(path, image_array)
     subI = (subI + 1) % 2
     subsubI = (subsubI + 1) % 2
+    if j == 1:
+        i+=1
+    if j == 0:
+        j+=1
+    else:
+        j=0
+    
 
 
 
